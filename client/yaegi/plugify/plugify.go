f536d54dd587234c24e8a9db7bb70ab191d8cedc
chenhg5 <chenhonggui@wps.cn>
2025-11-01 06:32:05 +0800

    update

--- client/yaegi/plugify/plugify.go
+++ client/yaegi/plugify/plugify.go
@@ -4,10 +4,15 @@ import (
 	"example.com/server/service"
 
 	goplugify "github.com/go-plugify/go-plugify"
+	ginadapter "github.com/go-plugify/webadapters/gin"
 )
 
 var BookService = new(service.BookService)
 
 var Logger = new(goplugify.DefaultLogger)
+
+var Util = new(goplugify.Util)
+
+var Ginengine = new(ginadapter.HttpRouter)
 
 type ServiceBook = service.Book
